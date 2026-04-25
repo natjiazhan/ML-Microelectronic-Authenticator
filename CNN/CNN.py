c4926a914f22e88efc3ce8ae8d1f65b6da796687
nhnguy27 <nhnguy27@asu.edu>
2026-04-25 08:35:28 -0700

    Add files via upload

---
+++ CNN/CNN.py
@@ -0,0 +1,124 @@
+import cv2
+import numpy as np
+from pathlib import Path
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+# Load images
+img_dir = Path("Images")
+image_extensions = {".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff"}
+
+read_array = []
+
+for path in sorted(img_dir.iterdir()):
+    if path.is_file() and path.suffix.lower() in image_extensions:
+        read_array.append(str(path))
+
+img = []
+for i in range(len(read_array)):
+    img.append(cv2.imread(read_array[i]))
+
+# targets
+Y = np.array([
+    0,0,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,
+    2,2,2,2,2,2,2,2,2,2,
+    3,3,3,3,3,3,3,3,3,3,
+    4,4,4,4,4,4,4,4,4,4,
+    5,5,5,5,5,5,5,5,5,5,
+    6,6,6,6,6,6,6,6,6,6,
+    7,7,7,7,7,7,7,7,7,7,
+    8,8,8,8,8,8,8,8,8,8,
+    9,9,9,9,9,9,9,9,9,9,
+])
+
+# Creates new images with small variations
+def image_gen(image):
+    h, w = image.shape[:2]
+    rand_angle = random.uniform(-5,5)
+    M_rot = cv2.getRotationMatrix2D((w/2, h/2),rand_angle,1.0)
+    M_rot[0,2] += random.randint(-5,5)
+    M_rot[1,2] += random.randint(-5,5)
+    gen_image = cv2.warpAffine(image, M_rot,(w, h),cv2.BORDER_CONSTANT,0)
+    return gen_image
+
+# Generate augmented dataset
+new_images = 5
+for i in range(len(img)):
+    for j in range(new_images):
+        new_image = image_gen(img[i])
+        img.append(new_image)
+        # assumes 10 images per class in original dataset
+        Y = np.append(Y,i//10)
+
+# split and prep datasets
+img = np.array(img)
+img = np.mean(img,axis=3)
+mean = img.mean()
+std = img.std()
+img = (img-mean)/std
+
+# 70/30 training/test split
+np.random.seed(1)
+idx = np.random.permutation(len(img))
+train_size = int(len(img)*0.7)
+train_idx = idx[:train_size]
+test_idx = idx[train_size:]
+# rearrange data to fit pytorch format
+X_train = torch.from_numpy(img[train_idx]).float().unsqueeze(1)
+X_test = torch.from_numpy(img[test_idx]).float().unsqueeze(1)
+Y_train = torch.from_numpy(Y[train_idx]).long()
+Y_test = torch.from_numpy(Y[test_idx]).long()
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3,padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3,padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3,padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((1, 1))
+        # out_features is the number of classes
+        self.fc = nn.Linear(64, 10)
+    # forward propagation
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.adaptive_pool(x)
+        x = x.view(x.size(0), -1)
+        return self.fc(x)
+
+cnn = CNN()
+lossFunc = nn.CrossEntropyLoss()
+optimizer = optim.Adam(cnn.parameters(), lr=0.002)
+epochs = 100
+batch_size = 8
+
+for i in range(epochs):
+    for j in range(0, len(X_train), batch_size):
+        # train in batches
+        inputs = X_train[j:j + batch_size]
+        labels = Y_train[j:j + batch_size]
+        optimizer.zero_grad()
+        outputs = cnn(inputs)
+        loss = lossFunc(outputs, labels)
+        loss.backward()
+        optimizer.step()
+    if i % 10 == 9:
+        print(i, loss.item())
+
+y_pred = torch.argmax(cnn(X_test), dim=1)
+errors = 0
+for i in range(len(y_pred)):
+    if y_pred[i] != Y_test[i]:
+        errors += 1
+accuracy = 1-errors/len(y_pred)
+print(y_pred)
+print(Y_test)
+print("Accuracy: ", accuracy)
+# save weights
+torch.save(cnn.state_dict(), "CNN_weights.pth")
